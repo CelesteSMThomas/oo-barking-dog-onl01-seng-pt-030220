7d3ac52e6e1ffda509a1ec4d01b7331fa89388ec
Celeste Thomas <celestesmthomas@gmail.com>
2020-03-16 18:56:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog
+  
+  def name=(dog_name)
+    @this_is_the_dogs_name
+  end
+  
+  def name
+    @this_is_the_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+    
+end
+
